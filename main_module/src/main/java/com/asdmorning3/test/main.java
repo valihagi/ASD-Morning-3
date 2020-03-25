6962db3846f02ec66a90b69509896aa7e0eef0c1
afissel <aaf.adri1ana.98>
2020-03-25 11:08:55 +0100

    LANG_004 first prototype of the study Interface[FA,IA]

--- main_module/src/main/java/com/asdmorning3/test/main.java
+++ main_module/src/main/java/com/asdmorning3/test/main.java
@@ -1,10 +1,12 @@
 package com.asdmorning3.test;
+import javax.swing.*;
 
 public class main {
 
 	public static void main (String[] args)
 	{
-		System.out.println("hello world");
+		studyInterface Interface = new studyInterface();
+
 	}
 
 }
